c5b023ecd0abf3bd2c772dc08c23bc13e7143dd3
David Elmer <david.elmer@gmail.com>
2022-03-15 12:59:52 -0400

    add location and start image

--- lib/screens/new_post_screen.dart
+++ lib/screens/new_post_screen.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart';
 
-class NewPostScreen extends StatelessWidget {
+class NewPostScreen extends StatefulWidget {
   const NewPostScreen({Key? key}) : super(key: key);
 
+  @override
+  State<NewPostScreen> createState() => _NewPostScreenState();
+}
+
+class _NewPostScreenState extends State<NewPostScreen> {
+
+  LocationData? locationData;
+  var locationService = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveLocation();
+  }
+
+  // retrieveLocation function from exploration - share_location_screen.dart
+  void retrieveLocation() async {
+    try {
+      var _serviceEnabled = await locationService.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await locationService.requestService();
+        if (!_serviceEnabled) {
+          print('Failed to enable service. Returning.');
+          return;
+        }
+      }
+
+      var _permissionGranted = await locationService.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await locationService.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          print('Location service permission not granted. Returning.');
+        }
+      }
+
+      locationData = await locationService.getLocation();
+    } on PlatformException catch (e) {
+      print('Error: ${e.toString()}, code: ${e.code}');
+      locationData = null;
+    }
+    locationData = await locationService.getLocation();
+    setState(() {});
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Post'),
-      ),
-      body: const Center(child: Text('New Post')),
-    );
+    if (locationData == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('New Post'),
+        ),
+        body: const Center(child: CircularProgressIndicator()),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('New Post'),
+        ),
+        body: Center(child: Text('Location: (${locationData?.latitude}, ${locationData?.longitude})')),
+      );
+    }
   }
-
 }
