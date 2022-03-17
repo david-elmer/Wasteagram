200dcd6793b1f4a4d0a234704d6669fd0ac0fdc7
David Elmer <david.elmer@gmail.com>
2022-03-17 13:28:56 -0400

    more cleanup

--- lib/components/post_list_view.dart
+++ lib/components/post_list_view.dart
@@ -4,16 +4,6 @@ import '../screens/detail_screen.dart';
 
 class PostListView extends StatelessWidget {
 
-  final posts = [
-    {
-      'date': '1/1/2020',
-      'image': 'image',
-      'numberOfItems': 5,
-      'latitude': 100,
-      'longitude': 100
-    }
-  ];
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -45,29 +35,5 @@ class PostListView extends StatelessWidget {
         }
       }
     );
-
   }
 }
-    // return ListView.builder(
-    //   itemCount: posts.length,
-    //   itemBuilder: (context, index) {
-    //     return ListTile(
-    //       title: Row(
-    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //         children: [
-    //           Text('${posts[index]['date']}'),
-    //           Text('${posts[index]['numberOfItems']}')
-    //         ],
-    //       ),
-    //       onTap: () {
-    //         Navigator.of(context).push(
-    //           MaterialPageRoute(
-    //             builder: (context) => DetailScreen(post: posts[index])
-    //           )
-    //         );
-    //       },
-    //     );
-    //   },
-    // );
-  //}
-
